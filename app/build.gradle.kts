466ff50529bc63a037f41193150d31398ef4eb6c
indigane <indigane@users.noreply.github.com>
2025-06-21 18:07:26 +0300

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "home.bluetooth_scanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "v1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
